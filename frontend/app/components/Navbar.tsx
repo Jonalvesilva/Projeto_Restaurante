dbf17087e52a7691aa03a60b9fc9b3640a4d5f42
Jonathan Silva <jonalves.silva@jsmail.com.br>
2024-02-02 13:45:06 -0300

    feat: alter navbar

--- frontend/app/components/Navbar.tsx
+++ frontend/app/components/Navbar.tsx
@@ -2,10 +2,9 @@
 import { Link, animateScroll as scroll } from "react-scroll";
 import { useState } from "react";
 import { AiOutlineClose } from "react-icons/ai";
-import { FaFacebook } from "react-icons/fa";
-import { FaInstagram } from "react-icons/fa";
 import { IoMdMenu } from "react-icons/io";
 import { LinkButton } from "./LinkButton";
+import data from "../../utils/restApi.json";
 
 export function Navbar() {
   const [nav, setNav] = useState(false);
@@ -20,8 +19,26 @@ export function Navbar() {
           <div className="text-3xl text-white flex items-center">
             Le Chateau Brasil
           </div>
-          <div className="hidden sm:flex items-center justify-center"></div>
-          <div className="flex sm:hidden items-center justify-center px-8">
+          <div className="hidden min-[900px]:flex items-center justify-center">
+            {data.data[0].navbarLinks.map((item) => {
+              return (
+                <Link
+                  activeClass="active"
+                  to={item.link}
+                  spy={true}
+                  smooth={true}
+                  offset={-200}
+                  duration={900}
+                  onClick={handleNav}
+                  className="text-2xl text-white px-4 cursor-pointer hover:text-orange-500 ease-in duration-100 "
+                  key={item.link}
+                >
+                  {item.title}
+                </Link>
+              );
+            })}
+          </div>
+          <div className="flex min-[900px]:hidden items-center justify-center px-8">
             <IoMdMenu
               size={30}
               className="text-white cursor-pointer"
@@ -35,7 +52,7 @@ export function Navbar() {
         <div
           className={
             nav
-              ? "sm:hidden fixed left-0 top-0 w-full h-screen bg-gray-200/85 z-10"
+              ? "min-[900px]:hidden fixed left-0 top-0 w-full h-screen bg-gray-200/85 z-10"
               : ""
           }
         >
@@ -48,76 +65,35 @@ export function Navbar() {
           >
             <div>
               <div className="flex w-full items-center justify-between">
-                <img
-                  src="/assets/logo.jfif"
-                  className="w-[150px] h-[150px]"
-                  alt="logo"
-                ></img>
+                <h2 className="text-2xl text-white">LE CHATEAU BRASIL</h2>
                 <div
                   onClick={handleNav}
-                  className="text-white rounded-full p-3 cursor-pointer "
+                  className="text-white rounded-full p-3 bg-orange-500 cursor-pointer "
                 >
-                  <AiOutlineClose size={30} />
+                  <AiOutlineClose size={20} />
                 </div>
               </div>
               <div className="flex flex-col items-center">
                 <ul className="uppercase flex flex-col mt-[150px] items-center">
-                  <li className="py-4" key="link1">
-                    <Link
-                      activeClass="active"
-                      to="sobre"
-                      spy={true}
-                      smooth={true}
-                      offset={-200}
-                      duration={900}
-                      onClick={handleNav}
-                      className="text-3xl text-white cursor-pointer hover:text-orange-500 ease-in duration-100 "
-                    >
-                      Sobre
-                    </Link>
-                  </li>
-                  <li onClick={handleNav} className="py-4" key="link2">
-                    <Link
-                      activeClass="active"
-                      to="unidades"
-                      spy={true}
-                      smooth={true}
-                      offset={-70}
-                      duration={700}
-                      onClick={handleNav}
-                      className="text-3xl text-white hover:text-orange-500 ease-in duration-100"
-                    >
-                      Unidades
-                    </Link>
-                  </li>
-                  <li className="py-4" key="link3">
-                    <Link
-                      activeClass="active"
-                      to="servicos"
-                      spy={true}
-                      smooth={true}
-                      offset={0}
-                      duration={900}
-                      onClick={handleNav}
-                      className="text-3xl text-white hover:text-orange-500 ease-in duration-100"
-                    >
-                      Serviços
-                    </Link>
-                  </li>
-                  <li className="py-4" key="link4">
-                    <Link
-                      activeClass="active"
-                      to="contato"
-                      spy={true}
-                      smooth={true}
-                      offset={-70}
-                      duration={900}
-                      onClick={handleNav}
-                      className="text-3xl text-white hover:text-orange-500 ease-in duration-100"
-                    >
-                      Contato
-                    </Link>
-                  </li>
+                  {data.data[0].navbarLinks.map((item) => {
+                    return (
+                      <li className="py-4" key={`li_${item.link}`}>
+                        <Link
+                          activeClass="active"
+                          to={item.link}
+                          spy={true}
+                          smooth={true}
+                          offset={-200}
+                          duration={900}
+                          onClick={handleNav}
+                          className="text-2xl text-white px-4 cursor-pointer hover:text-orange-500 ease-in duration-100 "
+                          key={item.link}
+                        >
+                          {item.title}
+                        </Link>
+                      </li>
+                    );
+                  })}
                 </ul>
               </div>
             </div>
